e0d260270470e1009b25f5ac6a2bf31b5f1183c0
shiyaamsundar <40712528+shiyaamsundar@users.noreply.github.com>
2020-12-02 19:04:23 +0530

    corss

--- src/index.ts
+++ src/index.ts
@@ -21,7 +21,7 @@ if (require.main === module) {
   const config = {
     rest: {
       port: +(process.env.PORT ?? 5000),
-      host: process.env.HOST || "https://timelineadminz.netlify.app/",
+      host: process.env.HOST,
       // The `gracePeriodForClose` provides a graceful close for http/https
       // servers with keep-alive clients. The default value is `Infinity`
       // (don't force-close). If you want to immediately destroy all sockets
